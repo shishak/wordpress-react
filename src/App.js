2b308e337bd8a74f642b8943bcd9de8734cb0d91
Shishak <semenshishak@gmail.com>
2019-05-12 06:02:35 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
     sections: [],
     current: null,
     dataRoute:
-      "http://35.188.114.84/wp-json/wp/v2/posts"
+      "https://cors.io/?http://35.188.114.84/wp-json/wp/v2"
   };
 
   get scaledSections() {
